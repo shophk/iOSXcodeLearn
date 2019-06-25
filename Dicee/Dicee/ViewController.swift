2c54d71e4e35dcefd6adffe040d870ecf37616eb
Mike <shophongkong@gmail.com>
2019-06-25 21:44:07 +0800

    Added new project Dicee for learning.

---
+++ Dicee/Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by WongMike on 25/6/2019.
+//  Copyright © 2019 WongMike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
