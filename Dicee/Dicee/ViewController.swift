5a5f0f94298abcc023cf1a747a63b5fdadd04116
Mike <shophongkong@gmail.com>
2019-06-27 22:51:50 +0800

    Added testing code for buttons click in Dicee app.

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var randomDiceIndex1 = 0
+    var randomDiceIndex2 = 0
+    
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     override func viewDidLoad() {
@@ -17,6 +20,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+        randomDiceIndex1 = Int.random(in: 0...5)
+        randomDiceIndex2 = Int.random(in: 0...5)
+        
+        print(randomDiceIndex1)
     }
     
 
